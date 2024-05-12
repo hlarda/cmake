1540593a1aad7baa85bca8f89e8ef71bbf1b03eb
hlarda <halareda63@gmail.com>
2024-05-12 05:11:33 +0300

    configure file

--- 4-lib/main.cpp
+++ 4-lib/main.cpp
@@ -1,7 +1,12 @@
 #include<lib.hpp>
-
+#include"config.h"
 int main()
 {
     function();
+    printf("Hello from main\n");
+    printf("Producer: %s\n", PRODUCER);
+    printf("Model  : %s\n", MODEL);
+    printf("Version: %d.%d\n", KITTY_VERSION_MAJOR, KITTY_VERSION_MINOR);
+
     return 0;
 }
